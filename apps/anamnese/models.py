cc84c80d5e97df59df39cd2945015e945434007d
Thiagosnts <39925677+Thiagosnts@users.noreply.github.com>
2021-10-21 21:00:41 -0300

    Replace nome

--- apps/anamnese/models.py
+++ apps/anamnese/models.py
@@ -4,55 +4,55 @@ database = utils.database_connection
 errors = utils.database_errors
 
 
-class Patient:
+class Question:
     def __init__(self):
         self.db = database
-        self.patients = self.db.patients
+        self.questions = self.db.questions
 
 
-    def edit_patient(self, dni, name, date_of_birth, email, phone, address, visit_reason):
-        patient = self.find_patient(dni)
-        if patient is None:
+    def edit_question(self, dni, name, date_of_birth, email, phone, address, visit_reason):
+        question = self.find_question(dni)
+        if question is None:
             return "Paciente não encontrado"
         try:
-            self.patients.update_one({'dni': dni}, {
+            self.questions.update_one({'dni': dni}, {
                 '$set': {'name': name, 'date_of_birth': date_of_birth, 'email': email, 'phone': phone,
                          'address': address, 'visit_reason': visit_reason}})
         except errors.OperationFailure:
             return "Oops, Paciente não atualizado"
         return True
 
-    def edit_patient_notes(self, dni, notes):
-        patient = self.find_patient(dni)
-        if patient is None:
+    def edit_question_notes(self, dni, notes):
+        question = self.find_question(dni)
+        if question is None:
             return "Paciente não encontrado"
         try:
-            self.patients.update_one({'dni': dni}, {'$set': {'notes': notes}})
+            self.questions.update_one({'dni': dni}, {'$set': {'notes': notes}})
         except errors.OperationFailure:
             return "Oops, Paciente não atualizado"
         return True
 
-    def delete_patient(self, dni):
-        patient = self.find_patient(dni)
-        if patient is None:
+    def delete_question(self, dni):
+        question = self.find_question(dni)
+        if question is None:
             return "Paciente não encontrado"
         try:
-            self.patients.update_one({'dni': dni}, {'$set': {'status': 0}})
+            self.questions.update_one({'dni': dni}, {'$set': {'status': 0}})
         except errors.OperationFailure:
             return "Oops, Paciente não deletado"
         return True
 
-    def list_patients(self, status=1):
+    def list_questions(self, status=1):
         query = {'status': status}
-        patients = self.patients.find(query)
-        count = patients.count()
+        questions = self.questions.find(query)
+        count = questions.count()
         if count > 0:
-            return patients
+            return questions
         return None
 
-    def get_patient_by_dni(self, dni, status=1):
+    def get_question_by_dni(self, dni, status=1):
         query = {'dni': dni, 'status': status}
-        patient = self.patients.find_one(query)
-        if patient is not None:
-            return patient
+        question = self.questions.find_one(query)
+        if question is not None:
+            return question
         return None
